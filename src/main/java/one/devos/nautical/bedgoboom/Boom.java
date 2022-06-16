ef7a443e9a84f55f3bf422d2ab4a3229a4ed52ae
Asoji <asoji-git@hiitsdevin.mozmail.com>
2022-06-16 13:13:45 -0700

    Logging + 1.18.2 and 1.19+ support

--- src/main/java/one/devos/nautical/bedgoboom/Boom.java
+++ src/main/java/one/devos/nautical/bedgoboom/Boom.java
@@ -2,14 +2,25 @@ package one.devos.nautical.bedgoboom;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 
 public class Boom implements ModInitializer {
     public static BoomConfig CONFIG;
 
+    public static final String MOD_ID = "bedgoboom";
+    public static final String VERSION = FabricLoader.getInstance().getModContainer(MOD_ID).get().getMetadata().getVersion().getFriendlyString();
+    public static final String MOD_NAME = FabricLoader.getInstance().getModContainer(MOD_ID).get().getMetadata().getName();
+
+    public static final Logger LOGGER = LogManager.getLogger(MOD_NAME);
+
     @Override
     public void onInitialize() {
+        LOGGER.info("[" + MOD_NAME + "] Version " + VERSION + " loaded." );
+        LOGGER.info("[" + MOD_NAME + "] " + "https://www.youtube.com/watch?v=uL4l1yRhTRY");
+
         CONFIG = new BoomConfig(FabricLoader.getInstance().getConfigDir().resolve("bedgoboom.properties"));
         try {
             CONFIG.initialize();
